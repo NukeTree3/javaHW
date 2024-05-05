01f1ec8cf7b38c2b8fb179e5bdd7ff764b5880c1
NukeTree3 <nuketree3@gmail.com>
2024-05-05 03:59:43 +0300

    Добавление удаления

--- telephonenote.java
+++ telephonenote.java
@@ -10,14 +10,14 @@ public class telephonenote {
         HashMap<String, String> map = new HashMap<>();
 
         //тестовые контакты
-        map.put("имя1", "45678987");
-        map.put("имя2", "98736254, 5625266, 5642572");
-        map.put("имя3", "63773773, 4567878");
-        map.put("имя4", "87655535");
+        map.put("name1", "45678987");
+        map.put("name2", "98736254, 5625266, 5642572");
+        map.put("name3", "63773773, 4567878");
+        map.put("name4", "87655535");
 
         Scanner scanner = new Scanner(System.in);
         while(true){
-            System.out.println("Укажете действие (1-добавление, 2-вывод всех контактов)");
+            System.out.println("Укажете действие (1-добавление, 2-вывод всех контактов, 3-удаление контакта)");
             String action = scanner.next();
 
             if (action.equals("1")){
@@ -48,8 +48,12 @@ public class telephonenote {
                     System.out.println(mapOutput.get(maxKey));
                     mapOutput.remove(maxKey);
                     System.out.println();
-                }
-                
+                }    
+            }
+            else if(action.equals("3")){
+                System.out.println("Укажете имя контакта");
+                String delName = scanner.next();
+                System.out.println("Удаление контакта: " + map.remove(delName));
             }else{
                 System.exit(0);
             }
